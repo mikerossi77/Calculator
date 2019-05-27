5d1f09b8e03763c5c4ab3560fc2cb35c00ff3399
Michael Rossi <michaelrossi1856@gmail.com>
2019-05-27 09:19:00 -0700

    trivia

--- calculator.js
+++ calculator.js
@@ -28,17 +28,37 @@ function numberClick(numberClicked) {
 }
 
 function operatorClick(operatorClicked) {
-
-    
     if (operatorClicked == "=") {
-        if(result == "1982"){
-            alert("1982 - UNC freshman Michael Jordan shoots the game winning shot to give the Tarheels the National Champtionship!")
+        if (Id == "UNC") {
+            if (result == "1982") {
+                alert("1982 - UNC freshman Michael Jordan shoots the game winning shot to give the Tarheels the National Champtionship!")
+            }
         }
-        if(result == "2000"){
-            alert("2000 - Freshman Michael Vick leads the Virginia Tech Hokies to the National Championship game with an undefeated regular season!")
+        if (Id == "VT") {
+            if (result == "2000") {
+                alert("2000 - Freshman Michael Vick leads the Virginia Tech Hokies to the National Championship game with an undefeated regular season!")
+            }
+            if (result == "1995") {
+                alert("1995 - BJ Ford runs unopposed for the presidency of the VA Zeta chapter of Sigma Alpha Epsilon. He wins with 10% of the vote.")
+            }
         }
-        if(result == "2019"){
-            alert("2019 - UC Irvine defeats Kansas State for first ever NCAA Tournament Game Win!")
+        if (Id == "UCI") {
+            if (result == "2019") {
+                alert("2019 - UC Irvine defeats Kansas State for first ever NCAA Tournament Game Win!")
+            }
+        }
+        if (Id == "Oso") {
+            if (result == "2017") {
+                alert("2017 - Katelyn and Taylor Rossi join the Oso Grande Grizzles as Kindergarteners!")
+            }
+            if (result == "2016") {
+                alert("2016 -Dylan Rossi makes his first appearance as an Oso Grande Grizzly for his Kindergarten year!")
+            }
+        }
+        if (Id == "Gonzaga") {
+            if (result == "1964") {
+                alert("1964 - Herman Joseph Rossi III graduates with a BA in History.  He would go on to a long and successful career in the Department of state, and would father four ungrateful children.")
+            }
         }
         result = myCalculator(tempResult, operation, result);
         newNumber = true;
@@ -98,13 +118,12 @@ function myCalculator(firstNumber, operator, secondNumber) {
     document.getElementById('result').value = answer;
     return answer;
 }
-function getQueryVariable(variable)
-{
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
 }
